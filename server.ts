a2c0e56f01b710abfd3167b766dbc883a3d54d4d
majidmovahedi <majidmovahedinasab@gmail.com>
2024-09-11 11:27:18 +0330

    add prettier and env to project

--- server.ts
+++ server.ts
@@ -1,10 +1,10 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript with Express!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, TypeScript with Express!");
 });
 
 app.listen(port, () => {
