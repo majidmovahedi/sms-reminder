d9fd4d9ae5e226d895e6b35f91680986f7640c48
majidmovahedi <majidmovahedinasab@gmail.com>
2024-09-17 19:02:15 +0330

    complete redis config & verify endpoint

--- src/controllers/api/v1/userController.ts
+++ src/controllers/api/v1/userController.ts
@@ -4,6 +4,7 @@ import jwt from 'jsonwebtoken';
 import { sendSMS } from '@utils/sms/sendSms';
 import { getRandomInt } from '@utils/sms/codeGenerator';
 import redisClient from '@configs/redisClient';
+import { Types } from 'mongoose';
 
 const JWT_SECRET = process.env.JWT_SECRET || 'your-secret-key';
 
@@ -33,7 +34,6 @@ export async function register(req: Request, res: Response) {
             generateCode.toString(),
         );
         await sendSMS(`Your Activation Code is ${generateCode}`, phoneNumber);
-
         return res.status(201).json({ message: 'User registered' });
     } catch (error) {
         return res.status(400).json(error);
@@ -65,6 +65,34 @@ export async function login(req: Request, res: Response) {
             expiresIn: '24h',
         });
         return res.json({ token });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Server error' });
+    }
+}
+
+export async function verify(req: Request, res: Response) {
+    try {
+        const { phoneNumber, code } = req.body;
+
+        // Find user by phone number
+        const user = await User.findOne({ phoneNumber });
+        if (!user) {
+            return res
+                .status(401)
+                .json({ error: 'phone number does not exist!' });
+        }
+        const userIdStr =
+            user._id instanceof Types.ObjectId ? user._id.toString() : user._id;
+
+        const storedOtp = await redisClient.get(userIdStr);
+
+        if (storedOtp === code) {
+            await User.findByIdAndUpdate(user, { isActive: true });
+            return res.json({ message: 'User verified successfully' });
+        } else {
+            res.status(400).json({ error: 'Invalid or expired OTP' });
+        }
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: 'Server error' });
--- src/routes/api/v1/user/userRouter.ts
+++ src/routes/api/v1/user/userRouter.ts
@@ -2,6 +2,7 @@ import {
     singleUser,
     login,
     register,
+    verify,
 } from '@controllers/api/v1/userController';
 import { authMiddleware } from '@middlewares/authMiddleware';
 import { Router } from 'express';
@@ -11,5 +12,6 @@ const router = Router();
 router.get('/', authMiddleware, singleUser);
 router.post('/register', register);
 router.post('/login', login);
+router.post('/verify', verify);
 
 export default router;
