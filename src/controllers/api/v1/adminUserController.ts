06c872f2bf5b265fb2b2feeafe07d339a49ad06a
majidmovahedi <majidmovahedinasab@gmail.com>
2024-09-25 18:34:37 +0330

    add singleUserConteoller endpoint

--- src/controllers/api/v1/adminUserController.ts
+++ src/controllers/api/v1/adminUserController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import User from '@models/userModel';
 import jwt from 'jsonwebtoken';
+import { ObjectId } from 'mongodb';
+import { string } from 'zod';
 
 export async function listUsersController(req: Request, res: Response) {
     try {
@@ -11,5 +13,25 @@ export async function listUsersController(req: Request, res: Response) {
         return res.status(200).json(users);
     } catch (err) {
         console.error('Error finding user:', err);
+    }
+}
+
+export async function singleUserController(req: Request, res: Response) {
+    try {
+        const { id } = req.params;
+
+        // Check if the provided ID is valid
+        if (!ObjectId.isValid(id)) {
+            return res.status(400).json({ error: 'Invalid user ID format.' });
+        }
+        // Find user by ID
+        const user = await User.findById({ _id: new ObjectId(id) });
+
+        if (!user) {
+            return res.json('This User Does Not Exist!');
+        }
+        return res.status(200).json(user);
+    } catch (err) {
+        console.error('Error finding user by ID:', err);
     }
 }
--- src/routes/api/v1/user/adminUserRouter.ts
+++ src/routes/api/v1/user/adminUserRouter.ts
@@ -1,4 +1,7 @@
-import { listUsersController } from '@controllers/api/v1/adminUserController';
+import {
+    listUsersController,
+    singleUserController,
+} from '@controllers/api/v1/adminUserController';
 import { adminMiddleware } from '@middlewares/authMiddleware';
 import { authMiddleware } from '@middlewares/authMiddleware';
 import { validate } from '@utils/validation/validate';
@@ -7,5 +10,6 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', authMiddleware, adminMiddleware, listUsersController);
+router.get('/:id', authMiddleware, adminMiddleware, singleUserController);
 
 export default router;
