a08ce0396e30063ac49e144f12a412d04434aa6b
majidmovahedi <majidmovahedinasab@gmail.com>
2024-09-29 17:52:20 +0330

    singlePlanController endpoint

--- src/controllers/api/v1/plan/adminPlanController.ts
+++ src/controllers/api/v1/plan/adminPlanController.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express';
 import Plan from '@models/planModel';
+import { ObjectId } from 'mongodb';
 
-export async function planController(req: Request, res: Response) {
+export async function plansController(req: Request, res: Response) {
     try {
         const plans = await Plan.find();
 
@@ -14,6 +15,28 @@ export async function planController(req: Request, res: Response) {
         return res.status(200).json(plans);
     } catch (err) {
         console.error('Error finding Plans:', err);
+        return res.status(500).json({ message: 'Server error occurred' });
+    }
+}
+
+export async function singlePlanController(req: Request, res: Response) {
+    try {
+        const { id } = req.params;
+
+        if (!ObjectId.isValid(id)) {
+            return res.status(400).json({ error: 'Invalid Plan ID format.' });
+        }
+        const plan = await Plan.findOne({ _id: new ObjectId(id) });
+
+        if (!plan) {
+            return res
+                .status(404)
+                .json({ message: 'This Plan Does Not Exist!' });
+        }
+
+        return res.status(200).json(plan);
+    } catch (err) {
+        console.error('Error finding Plan by ID:', err);
         return res.status(500).json({ message: 'Server error occurred' });
     }
 }
--- src/routes/api/v1/plan/adminPlanRouter.ts
+++ src/routes/api/v1/plan/adminPlanRouter.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express';
 import { authMiddleware, adminMiddleware } from '@middlewares/authMiddleware';
-import { planController } from '@controllers/api/v1/plan/adminPlanController';
+import {
+    plansController,
+    singlePlanController,
+} from '@controllers/api/v1/plan/adminPlanController';
 
 const router = Router();
 
 router.use(authMiddleware, adminMiddleware);
 
-// router.get('/:id',);
-router.get('/', planController);
+router.get('/:id', singlePlanController);
+router.get('/', plansController);
 // router.post('/', createReminderController);
 // router.put('/:id', updateReminderController);
 // router.delete('/:id', deleteReminderController);
