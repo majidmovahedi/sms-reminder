a74a1044c64c3be018dd3d6317c3a331adef9d56
majidmovahedi <majidmovahedinasab@gmail.com>
2024-09-30 18:12:48 +0330

    reminderValidationSchema

--- src/routes/api/v1/reminder/reminderRouter.ts
+++ src/routes/api/v1/reminder/reminderRouter.ts
@@ -6,6 +6,11 @@ import {
     updateReminderController,
 } from '@controllers/api/v1/reminder/reminderController';
 import { authMiddleware } from '@middlewares/authMiddleware';
+import {
+    CreateReminderSchema,
+    UpdateReminderSchema,
+} from '@utils/validation/reminderValidationSchema';
+import { validate } from '@utils/validation/validate';
 import { Router } from 'express';
 
 const router = Router();
@@ -14,8 +19,8 @@ router.use(authMiddleware);
 
 router.get('/:id', singleReminderController);
 router.get('/', remindersController);
-router.post('/', createReminderController);
-router.put('/:id', updateReminderController);
+router.post('/', validate(CreateReminderSchema), createReminderController);
+router.put('/:id', validate(UpdateReminderSchema), updateReminderController);
 router.delete('/:id', deleteReminderController);
 
 export default router;
