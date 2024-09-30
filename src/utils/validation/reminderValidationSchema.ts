7f7beffbbf48228839d00e7dffeca305e4886343
majidmovahedi <majidmovahedinasab@gmail.com>
2024-09-30 18:14:36 +0330

    updateReminderValidationSchema

--- src/utils/validation/reminderValidationSchema.ts
+++ src/utils/validation/reminderValidationSchema.ts
@@ -31,23 +31,34 @@ export const CreateReminderSchema = z.object({
 });
 
 export const UpdateReminderSchema = z.object({
-    fullname: z
+    title: z.string().min(3, 'title must be at least 3 characters').optional(),
+    reminderText: z
         .string()
-        .min(3, 'fullname must be at least 3 characters')
+        .min(3, 'reminderText must be at least 3 characters')
+        .max(60, 'reminderText must not exceed 60 characters')
         .optional(),
-    phoneNumber: z
-        .string()
-        .min(11, 'phoneNumber must be at least 11 characters')
-        .max(11, 'phoneNumber must not exceed 11 characters')
+    month: z
+        .number()
+        .int('Month must be an integer')
+        .min(1, 'The value must be a number between 1 and 12')
+        .max(12, 'The value must be a number between 1 and 12')
         .optional(),
-    isActive: z
-        .boolean({
-            invalid_type_error: 'isActive must be true or false',
-        })
+    day: z
+        .number()
+        .int('Day must be an integer')
+        .min(1, 'The value must be a number between 1 and 31')
+        .max(31, 'The value must be a number between 1 and 31')
         .optional(),
-    adminType: z
-        .boolean({
-            invalid_type_error: 'adminType must be true or false',
-        })
+    hour: z
+        .number()
+        .int('Hour must be an integer')
+        .min(0, 'The value must be a number between 0 and 23')
+        .max(23, 'The value must be a number between 0 and 23')
+        .optional(),
+    minute: z
+        .number()
+        .int('Minute must be an integer')
+        .min(0, 'The value must be a number between 0 and 60')
+        .max(60, 'The value must be a number between 0 and 60')
         .optional(),
 });
