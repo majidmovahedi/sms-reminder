4bb2c27e93db5405f22cde31cafa5a7141ac0245
Majid Movahedi <majidmovahedinasab@gmail.com>
2024-10-23 18:15:39 +0330

    change sendsms promise boolean

--- src/utils/sms/sendSms.ts
+++ src/utils/sms/sendSms.ts
@@ -5,7 +5,10 @@ dotenv.config();
 const lineNumber = process.env.SMS_LINE_NUMBER;
 const apiKey = process.env.SMS_API_KEY;
 
-export const sendSMS = async (msg: string, mobile: string) => {
+export const sendSMS = async (
+    msg: string,
+    mobile: string,
+): Promise<{ success: boolean }> => {
     const data = JSON.stringify({
         lineNumber: lineNumber,
         messageText: msg,
@@ -23,12 +26,16 @@ export const sendSMS = async (msg: string, mobile: string) => {
         data: data,
     };
 
-    axios(config)
-        .then(function (response) {
-            JSON.stringify(response.data);
-            // console.log(JSON.stringify(response.data));
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+    try {
+        const response = await axios(config);
+
+        if (response.data && response.data.isSuccessful) {
+            return { success: true };
+        } else {
+            return { success: false };
+        }
+    } catch (error) {
+        console.error('Error sending SMS:', error);
+        return { success: false };
+    }
 };
